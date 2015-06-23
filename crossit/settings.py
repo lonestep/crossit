87156b3f7c89809f6739ee0b487789baefaec73d
lonestep <lonestep@gmail.com>
2015-06-23 08:52:44 +0800

    statics

--- crossit/settings.py
+++ crossit/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'pc5za+-bcf8b@dvgeypi^=&hva8$5g$m__5+)pgsw=30)$f=@x'
+SECRET_KEY = '<SECRET_KEY>'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoUeditor',
     'oauth2_provider',
     'corsheaders',
     'crossit',
@@ -65,9 +66,9 @@ CORS_ORIGIN_WHITELIST = ('exmail.qq.com')
 DATABASES = {
     'default': {
         'ENGINE': 'mysql.connector.django',
-        'NAME': 'crossit_db',
-        'USER':'shawn',
-        'PASSWORD':'lkg4btlf',
+        'NAME': '<DB_NAME>',
+        'USER':'<DB_USR>',
+        'PASSWORD':'<DB_PSW>',
         'HOST':'',
         'PORT':'',
     }
